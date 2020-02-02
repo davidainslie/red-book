a52f3d4bf43806fe8905a45cd33ce549b218bf40
David Ainslie <dainslie@gmail.com>
2020-02-02 17:33:49 +0000

    Chap 3 - list

--- src/test/scala/com/backwards/chap3/Chap3Spec.scala
+++ src/test/scala/com/backwards/chap3/Chap3Spec.scala
@@ -1,6 +1,7 @@
 package com.backwards.chap3
 
 import scala.annotation.tailrec
+import cats.Monoid
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -32,20 +33,34 @@ class Chap3Spec extends AnyWordSpec with Matchers {
       if (as.isEmpty) Nil
       else Cons(as.head, apply(as.tail: _*))
 
-    /**
-     * A function that uses pattern matching to add up a list of integers.
-     * @param ints List[Int]
-     * @return Int
-     */
-    def sum(ints: List[Int]): Int = ints match {
-      case Nil => 0 // The sum of the empty list is 0.
-      case Cons(x, xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+    def sum[A: Monoid](xs: List[A]): A = {
+      import cats.syntax.monoid._
+
+      @tailrec
+      def sum(xs: List[A], acc: A): A = xs match {
+        case Nil => acc
+        case Cons(h, t) => sum(t, acc |+| h) // Monoid[A].combine(acc, h)
+      }
+
+      sum(xs, Monoid[A].empty)
+    }
+
+    def sumFoldLeft[A: Monoid](xs: List[A]): A = {
+      import cats.syntax.monoid._
+
+      foldLeft(xs, Monoid[A].empty)(_ |+| _) // Monoid[A].combine
     }
 
     def product(ds: List[Double]): Double = ds match {
       case Nil => 1.0
       case Cons(0.0, _) => 0.0
       case Cons(x, xs) => x * product(xs)
+    }
+
+    def productFoldLeft[A](xs: List[A])(implicit M: Monoid[A]): A = {
+      import cats.syntax.monoid._
+
+      foldLeft(xs, M.empty)(_ |+| _) // M.combine
     }
 
     def tail[A](xs: List[A]): List[A] = xs match {
@@ -75,6 +90,12 @@ class Chap3Spec extends AnyWordSpec with Matchers {
       case Cons(h, t) => Cons(h, append(t, ys))
     }
 
+    def appendUsingFoldLeft[A](xs: List[A], ys: List[A]): List[A] =
+      foldLeft(reverse(ys), xs)((xs, y) => Cons(y, xs))
+
+    def appendUsingFoldRight[A](xs: List[A], ys: List[A]): List[A] =
+      foldRight(xs, ys)((x, ys) => Cons(x, ys))
+
     def init[A](xs: List[A]): List[A] = xs match {
       case Nil => sys.error("Cannot 'init' Nil")
       case Cons(_, Nil) => Nil
@@ -91,9 +112,78 @@ class Chap3Spec extends AnyWordSpec with Matchers {
 
       go(xs, Nil)
     }
+
+    def foldRight[A, B](xs: List[A], z: B)(f: (A, B) => B): B = xs match {
+      case Nil =>
+        z
+
+      case Cons(h, t) =>
+        println(s"foldRight that will not short circuit for h = $h")
+        f(h, foldRight(t, z)(f))
+    }
+
+    def foldLeft[A, B](xs: List[A], z: B)(f: (B, A) => B): B = {
+      @tailrec
+      def foldLeft(xs: List[A], acc: B): B = xs match {
+        case Nil => acc
+        case Cons(h, t) => foldLeft(t, f(acc, h))
+      }
+
+      foldLeft(xs, z)
+    }
+
+    def foldLeftUsingFoldRight[A, B](xs: List[A], z: B)(f: (B, A) => B): B = {
+      // Easy way - just use reverse
+      // foldRight(reverse(xs), z)((a, b) => f(b, a))
+
+      // Other implementation builds up a chain of functions which, when called,
+      // results in the operations being performed with the correct associativity.
+      // We are calling `foldRight` with the `B` type being instantiated to `B => B`,
+      // then calling the built up function with the `z` argument.
+      def g: B => B = foldRight(xs, (b: B) => b)((a, g: B => B) =>
+        b => g(f(b, a))
+      )
+
+      g(z)
+    }
+
+    def foldRightUsingFoldLeft[A, B](xs: List[A], z: B)(f: (A, B) => B): B = {
+      // Easy way - just use reverse
+      // foldLeft(reverse(xs), z)((b, a) => f(a, b))
+
+      // Other implementation builds up a chain of functions which, when called,
+      // results in the operations being performed with the correct associativity.
+      // We are calling `foldLeft` with the `B` type being instantiated to `B => B`,
+      // then calling the built up function with the `z` argument.
+      def g: B => B = foldLeft(xs, (b: B) => b)((g: B => B, a) =>
+        b => g(f(a, b))
+      )
+
+      g(z)
+    }
+
+    def length[A](xs: List[A]): Int = {
+      @tailrec
+      def length(xs: List[A], count: Int): Int = xs match {
+        case Nil => count
+        case Cons(_, t) => length(t, count + 1)
+      }
+
+      length(xs, 0)
+    }
+
+    def lengthFoldLeft[A](xs: List[A]): Int =
+      foldLeft(xs, 0)((count, _) => count + 1)
+
+    def reverse[A](xs: List[A]): List[A] =
+      foldLeft(xs, Nil: List[A])((acc, x) => Cons(x, acc))
+
+    def concat[A](xs: List[List[A]]): List[A] =
+      foldLeftUsingFoldRight(xs, Nil: List[A])(append)
   }
 
   "3.1 What will be the result of the following match expression?" in {
+    import cats.instances.int._
     import List._
 
     val x = List(1, 2, 3, 4, 5) match {
@@ -156,5 +246,105 @@ class Chap3Spec extends AnyWordSpec with Matchers {
 
     `init better`(xs) mustBe List(1, 2, 3, 4)
     an [Exception] must be thrownBy `init better`(Nil)
+  }
+
+  "3.7 Short circuit foldRight" in {
+    // Can product, implemented using foldRight, immediately halt the recursion and return 0.0 if it encounters a 0.0?
+
+    import List._
+
+    val xs = List(1, 2, 3, 0, 5)
+
+    // Original implementation does not short circuit
+    foldRight(xs, 1)((a, b) => a * b) mustBe 0
+  }
+
+  "3.8" in {
+    import List._
+
+    val xs = List(1, 2, 3, 4, 5)
+
+    foldRight(xs, Nil: List[Int])(Cons(_, _)) mustBe xs
+  }
+
+  "3.9 Compute length of a list using foldRight" in {
+    val xs = List(1, 2, 3, 4, 5)
+
+    List.length(xs) mustBe 5
+  }
+
+  "3.10 foldLeft" in {
+    import List._
+
+    val xs = List(1, 2, 3, 4, 5)
+
+    foldLeft(xs, 0)(_ + _) mustBe 15
+  }
+
+  "3.11 sum, product, length using foldLeft" in {
+    import cats.instances.int._
+    import List._
+
+    val xs = List(1, 2, 3, 4, 5)
+
+    sumFoldLeft(xs) mustBe 15
+    productFoldLeft(xs)(IntProductMonoid) mustBe 120
+    lengthFoldLeft(xs) mustBe 5
+
+    object IntProductMonoid extends Monoid[Int] {
+      def empty: Int = 1
+
+      def combine(x: Int, y: Int): Int = x * y
+    }
+  }
+
+  "3.12" in {
+    import List._
+
+    reverse(List(1, 2, 3, 4, 5)) mustBe List(5, 4, 3, 2, 1)
+  }
+
+  "3.13 foldLeft in terms of foldRight and vice versa" in {
+    import List._
+
+    val xs = List(1, 2, 3, 4, 5)
+
+    foldLeftUsingFoldRight(xs, 0)((acc, x) => acc + x) mustBe 15
+    foldLeftUsingFoldRight(xs, 1)((acc, x) => acc * x) mustBe 120
+    foldLeftUsingFoldRight(List("h", "e", "l", "l", "o"), "")((acc, x) => acc + x) mustBe "hello"
+
+    foldRightUsingFoldLeft(xs, 0)((x, acc) => x + acc) mustBe 15
+    foldRightUsingFoldLeft(xs, 1)((x, acc) => x * acc) mustBe 120
+    foldRightUsingFoldLeft(List("h", "e", "l", "l", "o"), "")((x, acc) => x + acc) mustBe "hello"
+  }
+
+  "3.14 Implement append in terms of either foldLeft or foldRight" in {
+    import List._
+
+    val xs = List(1, 2, 3, 4, 5)
+
+    append(xs, xs) mustBe List(1, 2, 3, 4, 5, 1, 2, 3, 4, 5)
+    appendUsingFoldLeft(xs, xs) mustBe List(1, 2, 3, 4, 5, 1, 2, 3, 4, 5)
+    appendUsingFoldRight(xs, xs) mustBe List(1, 2, 3, 4, 5, 1, 2, 3, 4, 5)
+  }
+
+  "3.15 Concatenate a list of lists into a single list" in {
+    import List._
+
+    val xs = List(
+      List(1, 2, 3),
+      List(4),
+      List(5, 6)
+    )
+
+    concat(xs) mustBe List(1, 2, 3, 4, 5, 6)
+  }
+
+  "3.16" in {
+
+  }
+
+  "3.24" in {
+
   }
 }
