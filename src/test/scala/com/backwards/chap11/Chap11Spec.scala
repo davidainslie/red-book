a452ec8212e3b0e6ed084a05c1834376f267f550
David Ainslie <dainslie@gmail.com>
2020-05-16 22:47:15 +0100

    Chap 11 State Monad

--- src/test/scala/com/backwards/chap11/Chap11Spec.scala
+++ src/test/scala/com/backwards/chap11/Chap11Spec.scala
@@ -89,40 +89,127 @@ class Chap11Spec extends AnyWordSpec with Matchers {
       */
     }
 
-    "11.8" in {
+    "11.8 flatMap using compose and unit" in {
+      val ops: Option[Int] = optionMonad.flatMapUsingCompose(Some(1))(i => Some(i + 1))
 
+      ops mustBe Option(2)
     }
 
-    "11.9" in {
+    "11.12 join" in {
+      val ops: Option[Int] = optionMonad.join(Option(Option(42)))
 
+      ops mustBe Option(42)
     }
 
-    "11.10" in {
+    "11.13 flatMap using join and map" in {
+      val ops: Option[Int] = optionMonad.flatMapUsingJoinAndMap(Some(1))(i => Some(i + 1))
 
+      ops mustBe Option(2)
     }
 
-    "11.11" in {
+    "11.17 Identity Monad" in {
+      val identityMonad = new Monad[Id] {
+        def unit[A](a: => A): Id[A] = Id(a)
 
+        def flatMap[A, B](ma: Id[A])(f: A => Id[B]): Id[B] = ma flatMap f
+      }
+
+      val x: Id[Int] = identityMonad.map(Id(42))(_ + 1)
+
+      x mustBe Id(43)
+
+      val y: Id[Int] = identityMonad.flatMap(Id(42))(i => Id(i + 1))
+
+      y mustBe Id(43)
+    }
+  }
+
+  "State Monad" should {
+    case class State[S, A](run: S => (A, S)) {
+      def map[B](f: A => B): State[S, B] =
+        State { s =>
+          val (a, s1) = run(s)
+          (f(a), s1)
+        }
+
+      def flatMap[B](f: A => State[S, B]): State[S, B] =
+        State { s =>
+          val (a, s1) = run(s)
+          f(a).run(s1)
+        }
     }
 
-    "11.12" in {
+    /*
+    It looks like State fits the profile for being a monad.
+    But its type constructor takes two type arguments, and Monad requires a type constructor of one argument, so we can’t just say Monad[State].
+    But if we choose some particular S, then we have something like State[S, _], which is the kind of thing expected by Monad.
+    So State doesn’t just have one monad instance but a whole family of them, one for each choice of S.
+    We’d like to be able to partially apply State to where the S type argument is fixed to be some concrete type.
 
+    This is much like how we might partially apply a function, except at the type level.
+    E.g. we can create an IntState type constructor, which is an alias for State with its first type argument fixed to be Int:
+    */
+
+    type IntState[A] = State[Int, A]
+
+    // And IntState is exactly the kind of thing that we can build a Monad for:
+    object IntStateMonad extends Monad[IntState] {
+      def unit[A](a: => A): IntState[A] = State(s => (a, s))
+
+      def flatMap[A,B](st: IntState[A])(f: A => IntState[B]): IntState[B] =
+        st flatMap f
     }
 
-    "11.13" in {
+    /*
+    Of course, it would be really repetitive if we had to manually write a separate Monad instance for each specific state type.
+    Unfortunately, Scala doesn’t allow us to use underscore syntax to simply say State[Int, _] to create an anonymous type constructor like we create anonymous functions.
+    But instead we can use something similar to lambda syntax at the type level. For example, we could have declared IntState directly inline like this:
+    */
 
+    object IntStateMonadViaLambda extends Monad[({ type IntState[A] = State[Int, A] })#IntState] {
+      def unit[A](a: => A): IntState[A] = State(s => (a, s))
+
+      def flatMap[A,B](st: IntState[A])(f: A => IntState[B]): IntState[B] =
+        st flatMap f
     }
 
-    "11.14" in {
+    /*
+    All we’re doing is declaring an anonymous type within parentheses.
+    This anonymous type has, as one of its members, the type alias IntState, which looks just like before.
+    Outside the parentheses we’re then accessing its IntState member with the # syntax.
+    Just like we can use a dot (.) to access a member of an object at the value level, we can use the # symbol to access a type member.
 
+    A type constructor declared inline like this is often called a type lambda in Scala.
+    We can use this trick to partially apply the State type constructor and declare a StateMonad trait.
+    An instance of StateMonad[S] is then a monad instance for the given state type S:
+    */
+
+    def stateMonad[S] = new Monad[({ type M[x] = State[S,x] })#M] {
+      def unit[A](a: => A): State[S,A] = State(s => (a, s))
+
+      def flatMap[A,B](st: State[S,A])(f: A => State[S,B]): State[S,B] = st flatMap f
     }
 
-    "11.15" in {
+    // Note we can use Kind Projector to actually achieve what we want.
 
+    object IntStateMonadViaKindProjectorVersionOfLambda extends Monad[State[Int, *]] {
+      def unit[A](a: => A): IntState[A] = State(s => (a, s))
+
+      def flatMap[A,B](st: IntState[A])(f: A => IntState[B]): IntState[B] =
+        st flatMap f
     }
+  }
 
-    "11.16" in {
+  "Reader Monad" should {
+    case class Reader[R, A](run: R => A)
 
+    object Reader {
+      def readerMonad[R]: Monad[Reader[R, *]] = new Monad[Reader[R, *]] {
+        def unit[A](a: => A): Reader[R, A] = Reader(_ => a)
+
+        def flatMap[A, B](st: Reader[R, A])(f: A => Reader[R, B]): Reader[R, B] =
+          Reader(r => f(st.run(r)).run(r))
+      }
     }
   }
 }
@@ -156,6 +243,12 @@ trait Monad[F[_]] extends Functor[F] {
   def unit[A](a: => A): F[A]
 
   def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B]
+
+  def flatMapUsingCompose[A, B](ma: F[A])(f: A => F[B]): F[B] =
+    compose((_: Unit) => ma, f)(())
+
+  def flatMapUsingJoinAndMap[A, B](ma: F[A])(f: A => F[B]): F[B] =
+    join(map(ma)(f))
 
   def map[A, B](ma: F[A])(f: A => B): F[B] =
     flatMap(ma)(a => unit(f(a)))
@@ -204,6 +297,9 @@ trait Monad[F[_]] extends Functor[F] {
       compose(f, (b: Boolean) => if (b) map2(unit(a), acc)(_ +: _) else acc)(a)
     }
   }
+
+  def join[A](mma: F[F[A]]): F[A] =
+    flatMap(mma)(identity)
 }
 
 object Monad {
@@ -252,4 +348,11 @@ object Monad {
         ma.flatMap(f)
     }
   }
+}
+
+// Identity Monad:
+final case class Id[A](value: A) {
+  def flatMap[B](f: A => Id[B]): Id[B] = f(value)
+
+  def map[B](f: A => B): Id[B] = Id(f(value))
 }
---
+++ src/test/scala/com/backwards/chap12/Chap12Spec.scala
@@ -0,0 +1,8 @@
+package com.backwards.chap12
+
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class Chap12Spec extends AnyWordSpec with Matchers {
+
+}
