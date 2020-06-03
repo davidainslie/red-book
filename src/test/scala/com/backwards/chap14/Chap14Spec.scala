117ea3a54999993e420b08a5223b75c649778ccb
David Ainslie <dainslie@gmail.com>
2020-06-03 15:39:10 +0100

    Chap 14 Mutable reference

--- src/test/scala/com/backwards/chap14/Chap14Spec.scala
+++ src/test/scala/com/backwards/chap14/Chap14Spec.scala
@@ -21,6 +21,25 @@ class Chap14Spec extends AnyWordSpec with Matchers {
         b <- r2.read
       } yield (a, b)
     }
+
+    "illustrate idea safely" in {
+      val p: RunnableST[(Int, Int)] = new RunnableST[(Int, Int)] {
+        def apply[S]: ST[S, (Int, Int)] = for {
+          r1 <- STRef(1)
+          r2 <- STRef(2)
+          x <- r1.read
+          y <- r2.read
+          _ <- r1.write(y + 1)
+          _ <- r2.write(x + 1)
+          a <- r1.read
+          b <- r2.read
+        } yield (a, b)
+      }
+
+      val r = ST runST p
+
+      r mustBe (3, 2)
+    }
   }
 }
 
@@ -55,6 +74,11 @@ object ST {
       def run(s: S): (A, S) = (memo, s)
     }
   }
+
+  def runST[A](st: RunnableST[A]): A =
+    st[Unit].run(())._1
+    // OR explicitly:
+    // st.apply[Unit].run(())._1
 }
 
 /**
@@ -80,4 +104,53 @@ object STRef {
         var cell: A = a
       }
     )
+}
+
+trait RunnableST[A] {
+  def apply[S]: ST[S, A]
+}
+
+sealed abstract class STArray[S, A: Manifest] {
+  protected def value: Array[A]
+
+  def size: ST[S, Int] = ST(value.length)
+
+  // Write a value at the give index of the array
+  def write(i: Int, a: A): ST[S, Unit] = new ST[S, Unit] {
+    def run(s: S): (Unit, S) = {
+      value(i) = a
+      ((), s)
+    }
+  }
+
+  // Read the value at the given index of the array
+  def read(i: Int): ST[S, A] = ST(value(i))
+
+  // Turn the array into an immutable list
+  def freeze: ST[S, List[A]] = ST(value.toList)
+
+  def fill(xs: Map[Int, A]): ST[S, Unit] =
+    xs.foldRight(ST[S, Unit](())) {
+      case ((k, v), st) => st flatMap (_ => write(k, v))
+    }
+
+  def swap(i: Int, j: Int): ST[S, Unit] = for {
+    x <- read(i)
+    y <- read(j)
+    _ <- write(i, y)
+    _ <- write(j, x)
+  } yield ()
+}
+
+object STArray {
+  // Construct an array of the given size filled with the value v
+  def apply[S,A:Manifest](sz: Int, v: A): ST[S, STArray[S, A]] =
+    ST(new STArray[S, A] {
+      lazy val value: Array[A] = Array.fill(sz)(v)
+    })
+
+  def fromList[S, A: Manifest](xs: List[A]): ST[S, STArray[S, A]] =
+    ST(new STArray[S, A] {
+      lazy val value: Array[A] = xs.toArray
+    })
 }
