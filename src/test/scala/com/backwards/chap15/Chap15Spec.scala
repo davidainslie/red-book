70c99325f2cf2b3d89f6e27aae9bc8bdab3eaf4c
David Ainslie <dainslie@gmail.com>
2020-06-07 16:26:00 +0100

    Chap 15 Streaming

--- src/test/scala/com/backwards/chap15/Chap15Spec.scala
+++ src/test/scala/com/backwards/chap15/Chap15Spec.scala
@@ -61,7 +61,7 @@ class Chap15Spec extends AnyWordSpec with Matchers {
   }
 
   "Process" should {
-    "liftOne" in new Version1 {
+    "liftOne" in new Simple {
       import Process._
 
       val p: Process[Int, Int] = liftOne((x: Int) => x * 2)
@@ -77,7 +77,7 @@ class Chap15Spec extends AnyWordSpec with Matchers {
       */
     }
 
-    "filter" in new Version1 {
+    "filter" in new Simple {
       import Process._
 
       val even: Process[Int, Int] = filter((x: Int) => x % 2 == 0)
@@ -87,7 +87,7 @@ class Chap15Spec extends AnyWordSpec with Matchers {
       evens mustBe List(2, 4)
     }
 
-    "sum" in new Version1 {
+    "sum" in new Simple {
       import Process._
 
       val s: List[Double] = sum(LazyList(1.0, 2.0, 3.0, 4.0)).toList
@@ -95,7 +95,7 @@ class Chap15Spec extends AnyWordSpec with Matchers {
       s mustBe List(1.0, 3.0, 6.0, 10.0)
     }
 
-    "15.1a take" in new Version1 {
+    "15.1a take" in new Simple {
       import Process._
 
       val process: Process[Int, Int] = take[Int](2)
@@ -103,7 +103,7 @@ class Chap15Spec extends AnyWordSpec with Matchers {
       process(LazyList(1, 2, 3, 4)).toList mustBe List(1, 2)
     }
 
-    "15.1b drop" in new Version1 {
+    "15.1b drop" in new Simple {
       import Process._
 
       val process: Process[Int, Int] = drop[Int](1)
@@ -111,7 +111,7 @@ class Chap15Spec extends AnyWordSpec with Matchers {
       process(LazyList(1, 2, 3, 4)).toList mustBe List(2, 3, 4)
     }
 
-    "15.1c takeWhile" in new Version1 {
+    "15.1c takeWhile" in new Simple {
       import Process._
 
       val process: Process[Int, Int] = takeWhile[Int](_ < 4)
@@ -119,7 +119,7 @@ class Chap15Spec extends AnyWordSpec with Matchers {
       process(LazyList(1, 2, 3, 4)).toList mustBe List(1, 2, 3)
     }
 
-    "15.1d dropWhile" in new Version1 {
+    "15.1d dropWhile" in new Simple {
       import Process._
 
       val process: Process[Int, Int] = dropWhile[Int](_ < 3)
@@ -127,7 +127,7 @@ class Chap15Spec extends AnyWordSpec with Matchers {
       process(LazyList(1, 2, 3, 4)).toList mustBe List(3, 4)
     }
 
-    "15.2 count" in new Version1 {
+    "15.2 count" in new Simple {
       import Process._
 
       val process: Process[Int, Int] = count[Int]
@@ -135,7 +135,7 @@ class Chap15Spec extends AnyWordSpec with Matchers {
       process(LazyList(1, 2, 3, 4)).last mustBe 4
     }
 
-    "15.3 mean" in new Version1 {
+    "15.3 mean" in new Simple {
       import Process._
 
       val process: Process[Double, Double] = mean
@@ -144,7 +144,7 @@ class Chap15Spec extends AnyWordSpec with Matchers {
       process(LazyList(1, 2, 3, 4)).last mustBe 2.5
     }
 
-    "15.4a sum using loop" in new Version1 {
+    "15.4a sum using loop" in new Simple {
       import Process._
 
       val s: List[Double] = sumUsingLoop(LazyList(1.0, 2.0, 3.0, 4.0)).toList
@@ -152,7 +152,7 @@ class Chap15Spec extends AnyWordSpec with Matchers {
       s mustBe List(1.0, 3.0, 6.0, 10.0)
     }
 
-    "15.4b count using loop" in new Version1 {
+    "15.4b count using loop" in new Simple {
       import Process._
 
       val process: Process[Int, Int] = countUsingLoop[Int]
@@ -160,7 +160,7 @@ class Chap15Spec extends AnyWordSpec with Matchers {
       process(LazyList(1, 2, 3, 4)).last mustBe 4
     }
 
-    "15.5 pipe" in new Version1 {
+    "15.5 pipe" in new Simple {
       import Process._
 
       val p1: Process[Double, Double] = sum
@@ -175,25 +175,42 @@ class Chap15Spec extends AnyWordSpec with Matchers {
       (filter[Int](_ % 2 == 0) |> lift(_ + 1))(LazyList(1, 2, 3, 4)).toList mustBe List(3, 5)
     }
 
-    "15.6 zipWithIndex" in new Version1 {
+    "15.6 zipWithIndex" in new Simple {
       import Process._
+
+      val even: Process[Int, (Int, Int)] = filter((x: Int) => x % 2 == 0).zipWithIndex
+
+      even(LazyList(1, 2, 3, 4)).toList mustBe List(2 -> 0, 4 -> 1)
     }
 
-    "15.7 mean in terms of a combinator using sum and count" in new Version1 {
+    "15.7 mean in terms of a combinator using sum and count" in new Simple {
       import Process._
+
+      val mean: Process[Double, Double] = sum.zip(count).map {
+        case (sum, count) => sum / count
+      }
+
+      mean(LazyList(1, 2, 3, 4)).toList mustBe List(1.0, 1.5, 2.0, 2.5)
+      mean(LazyList(1, 2, 3, 4)).last mustBe 2.5
     }
 
-    "15.8 exists" in new Version1 {
+    "15.8 exists" in new Simple {
       import Process._
+
+      val process: Process[Int, Boolean] = exists[Int](_ >= 4)
+
+      process(LazyList(1, 2, 3, 4)).last mustBe true
     }
 
-    "15.9 toCelsius" in new Version1 {
+    "15.9 toCelsius" in new Simple {
       import Process._
+
+      convertFahrenheit(LazyList("75", "", "60", "#", "90")).toList.map(_.toInt) mustBe List(23, 15, 32)
     }
   }
 
   "Back to counting line numbers but now with monadic streams" should {
-    "boil down the counting example to: count |> exists(_ > 40000)" in new Version1 {
+    "boil down the counting example to: count |> exists(_ > 40000)" in new Simple {
       import Process._
 
       def processFile[A, B](
@@ -222,12 +239,11 @@ class Chap15Spec extends AnyWordSpec with Matchers {
 
       // We can now solve the original problem with the following:
       processFile(new File("build.sbt"), count |> exists(_ > 40000), false)(_ || _)
-
     }
   }
 }
 
-trait Version1 {
+trait Simple {
   /**
    * A Process[I, O] can be used to transform a stream containing I values to a stream of O values.
    * But Process[I, O] isn’t a typical function Stream[I] => Stream[O], which could consume the input stream and construct the output stream.
@@ -317,6 +333,12 @@ trait Version1 {
       case Emit(h, t) => f(h) ++ t.flatMap(f)
       case Await(recv) => Await(recv andThen (_ flatMap f))
     }
+
+    def zip[O2](p: Process[I, O2]): Process[I, (O, O2)] =
+      Process.zip(this, p)
+
+    def zipWithIndex: Process[I, (O, Int)] =
+      this zip (count map (_ - 1))
   }
 
   /**
@@ -455,5 +477,208 @@ trait Version1 {
 
       go(0.0, 0.0)
     }
+
+    def feed[A, B](oa: Option[A])(p: Process[A, B]): Process[A, B] =
+      p match {
+        case Halt() => p
+        case Emit(h, t) => Emit(h, feed(oa)(t))
+        case Await(recv) => recv(oa)
+      }
+
+    /*
+     * Can you think of a generic combinator that would allow for the definition of `mean` in terms of `sum` and `count`?
+     *
+     * Yes, it is `zip`, which feeds the same input to two processes.
+     * The implementation is a bit tricky, as we have to make sure that input gets fed to both `p1` and `p2`.
+     */
+    def zip[A, B, C](p1: Process[A, B], p2: Process[A, C]): Process[A, (B, C)] =
+      (p1, p2) match {
+        case (Halt(), _) =>
+          Halt()
+
+        case (_, Halt()) =>
+          Halt()
+
+        case (Emit(b, t1), Emit(c, t2)) =>
+          Emit((b, c), zip(t1, t2))
+
+        case (Await(recv1), _) =>
+          Await((oa: Option[A]) => zip(recv1(oa), feed(oa)(p2)))
+
+        case (_, Await(recv2)) =>
+          Await((oa: Option[A]) => zip(feed(oa)(p1), recv2(oa)))
+      }
+
+    /*
+     * We choose to emit all intermediate values, and not halt.
+     * See `existsResult` below for a trimmed version.
+     */
+    def exists[I](f: I => Boolean): Process[I, Boolean] =
+      lift(f) |> any
+
+    /* Emits whether a `true` input has ever been received. */
+    def any: Process[Boolean, Boolean] =
+      loop(false)((b: Boolean, s) => (s || b, s || b))
+
+    /* A trimmed `exists`, containing just the final result. */
+    /*def existsResult[I](f: I => Boolean) =
+      exists(f) |> takeThrough(!_) |> dropWhile(!_) |> echo.orElse(emit(false))*/
+
+    /*
+    Write a program that reads degrees fahrenheit as `Double` values from a file, converts each temperature to celsius, and writes results to another file.
+
+    This process defines the here is core logic, a transducer that converts
+    input lines (assumed to be temperatures in degrees fahrenheit)
+    to output lines (temperatures in degress celsius).
+    Left as an exercise to supply another wrapper like `processFile` to actually do the IO and drive the process.
+    */
+    def convertFahrenheit: Process[String, Double] =
+      filter[String](line => !line.startsWith("#")) |> filter(line => line.trim.nonEmpty) |> lift(line => toCelsius(line.toDouble))
+
+    def toCelsius(fahrenheit: Double): Double =
+      (5.0 / 9.0) * (fahrenheit - 32.0)
+  }
+}
+
+trait Extentible {
+  /*
+  Our generalized process type is parameterized on the protocol used for communicating with the driver.
+  This works similarly to the `IO` type.
+  The `Await` constructor emits a request of type `F[A]`, and receives a response of type `Either[Throwable, A]`:
+
+  The `Await` constructor may now receive a successful result or an error.
+
+  The `Halt` constructor now has a _reason_ for termination, which may be either normal termination indicated by the special exception `End`,
+  forceful terimation, indicated by the special exception `Kill`, or some other error.
+
+  We'll use the improved `Await` and `Halt` cases together to ensure that all resources get released, even in the event of exceptions.
+  */
+  trait Process[F[_], O] {
+    import Process._
+
+    def ++(p: => Process[F,O]): Process[F,O] =
+      this.onHalt {
+        case End => Try(p) // we consult `p` only on normal termination
+        case err => Halt(err)
+      }
+
+    /*
+     * Like `++`, but _always_ runs `p`, even if `this` halts with an error.
+     */
+    def onComplete(p: => Process[F, O]): Process[F, O] =
+      this.onHalt {
+        case End => p.asFinalizer
+        case err => p.asFinalizer ++ Halt(err) // we always run `p`, but preserve any errors
+      }
+
+    def asFinalizer: Process[F, O] = this match {
+      case Emit(h, t) =>
+        Emit(h, t.asFinalizer)
+
+      case Halt(e) =>
+        Halt(e)
+
+      case Await(req,recv) => await(req) {
+        case Left(Kill) => this.asFinalizer
+        case x => recv(x)
+      }
+    }
+
+    def onHalt(f: Throwable => Process[F, O]): Process[F, O] = this match {
+      case Halt(e) => Try(f(e))
+      case Emit(h, t) => Emit(h, t.onHalt(f))
+      case Await(req, recv) => Await(req, recv andThen (_.onHalt(f)))
+    }
+
+    // Many of the same operations can be defined for this generalized `Process` type, regardless of the choice of `F`.
+    def map[O2](f: O => O2): Process[F, O2] = this match {
+      case Await(req, recv) =>
+        Await(req, recv andThen (_ map f))
+
+      case Emit(h, t) =>
+        Try { Emit(f(h), t map f) }
+
+      case Halt(err) =>
+        Halt(err)
+    }
+
+    /*
+     * Anywhere we _call_ `f`, we catch exceptions and convert them to `Halt`.
+     * See the helper function `Try` defined below.
+     */
+    def flatMap[O2](f: O => Process[F, O2]): Process[F, O2] =
+      this match {
+        case Halt(err) =>
+          Halt(err)
+
+        case Emit(o, t) =>
+          Try(f(o)) ++ t.flatMap(f)
+
+        case Await(req, recv) =>
+          Await(req, recv andThen (_ flatMap f))
+      }
+
+    def runLog(src: Process[IO, O]): IO[IndexedSeq[O]] = IO {
+      val E = java.util.concurrent.Executors.newFixedThreadPool(4)
+
+      @tailrec
+      def go(cur: Process[IO, O], acc: IndexedSeq[O]): IndexedSeq[O] =
+        cur match {
+          case Emit(h,t) =>
+            go(t, acc :+ h)
+
+          case Halt(End) =>
+            acc
+
+          case Halt(err) =>
+            throw err
+
+          case Await(req, recv) =>
+            val next =
+              try recv(Right(req.unsafeRunSync))
+              catch { case err: Throwable => recv(Left(err)) }
+
+            go(next, acc)
+        }
+
+      try go(src, IndexedSeq())
+      finally E.shutdown()
+    }
+  }
+
+  object Process {
+    case class Await[F[_], A, O](
+      req: F[A],
+      recv: Throwable Either A => Process[F, O]
+    ) extends Process[F, O]
+
+    case class Emit[F[_], O](
+      head: O,
+      tail: Process[F, O]
+    ) extends Process[F, O]
+
+    case class Halt[F[_], O](err: Throwable) extends Process[F, O]
+
+    /* Special exception indicating normal termination */
+    case object End extends Exception
+
+    /* Special exception indicating forceful termination */
+    case object Kill extends Exception
+
+    /**
+     * Helper function to safely produce `p`, or gracefully halt with an error if an exception is thrown.
+     */
+    def Try[F[_], O](p: => Process[F, O]): Process[F, O] =
+      try p
+      catch { case e: Throwable => Halt(e) }
+
+    def emit[F[_], O](
+      head: O,
+      tail: Process[F, O] = Halt[F, O](End)
+    ): Process[F, O] =
+      Emit(head, tail)
+
+    def await[F[_], A, O](req: F[A])(recv: Throwable Either A => Process[F, O]): Process[F, O] =
+      Await(req, recv)
   }
 }
